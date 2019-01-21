240a7ec43af443d92178b68810da0a60593396ef
Hajredin Abdiu <diniabdiu@gmail.com>
2019-01-21 19:13:46 +0100

    display repos

--- github.js
+++ github.js
@@ -2,12 +2,20 @@ class Github {
     constructor() {
         this.client_id = 'acfe04bc1844e4679f2a';
         this.client_secret = '625087dd77afa80d7bee16fd8c3b994903cbfc0f';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc'
     }
     async getUser(user) {
         const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`http://api.github.com/users/${user}/repos?per_pages=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
         return{
-            profile
+            profile,
+            repos
         }
     }
 }
--- ui.js
+++ ui.js
@@ -29,7 +29,30 @@ class UI {
             <div id="repos"></div>
         `;
     }
-    showAlert(message, className){
+    showRepos(repos) {
+        let output = '';
+        repos.forEach(function (repo) {
+            output += `
+                <div class="card card-body mb-2">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                        </div>
+                        <div class="col-md-6">
+                        <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                        <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+                        <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+        });
+
+        document.getElementById('repos').innerHTML = output;
+    }
+
+    showAlert(message, className) {
         this.clearAlert();
         const div = document.createElement('div');
         div.className = className;
@@ -45,12 +68,12 @@ class UI {
 
     clearAlert() {
         const currentAlert = document.querySelector('.alert');
-        if(currentAlert) {
+        if (currentAlert) {
             currentAlert.remove();
         }
     }
 
-    clearProfile(){
+    clearProfile() {
         this.profile.innerHTML = '';
     }
 }
