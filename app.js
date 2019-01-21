d6f1786ebf0227b165e6b4760df6a017fda82254
Hajredin Abdiu <diniabdiu@gmail.com>
2019-01-21 18:19:47 +0100

    show alert

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ searchUser.addEventListener('keyup', (e) => {
         github.getUser(userText)
         .then(data => {
            if(data.profile.message === 'Not Found') {
-                // alert('')
+                ui.showAlert('User not found', 'alert alert-danger');
            }else {
             ui.showProfile(data.profile);
            }
         })
     }else {
-
+        ui.clearProfile();
     }
 });
--- github.js
+++ github.js
@@ -1,10 +1,10 @@
 class Github {
     constructor() {
         this.client_id = 'acfe04bc1844e4679f2a';
-        this.secret_id = '625087dd77afa80d7bee16fd8c3b994903cbfc0f';
+        this.client_secret = '625087dd77afa80d7bee16fd8c3b994903cbfc0f';
     }
     async getUser(user) {
-        const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}*client_secret=${this.client_secret}`);
+        const profileResponse = await fetch(`http://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
         const profile = await profileResponse.json();
         return{
             profile
